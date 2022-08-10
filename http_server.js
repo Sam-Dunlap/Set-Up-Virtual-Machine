d9884678c61f7c1d7d185509a042453c2437823f
Sam-Dunlap <sam.dunlap15@gmail.com>
2022-08-09 22:53:36 -0700

    updated app.listen

--- http_server.js
+++ http_server.js
@@ -48,6 +48,6 @@ app.post('/add', function(req, res){
 
 // start server
 // -----------------------
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log('Running on port 3000!')
 })
